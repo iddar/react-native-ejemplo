8896394472306dd4ccebc02a2f346accc3a80c7e
Iddar <iddar@dbug.mx>
2017-06-30 16:30:32 -0500

    finihs

--- actions/api.js
+++ actions/api.js
@@ -1,6 +1,13 @@
 
 import * as fake from '../lib/fakeAPI'
 
+// login fake.login(user: 'string', password: 'string > 8')
+// Get all items fake.list()
+// fake.ticket([
+//   {id: 'D08', count: 3},
+//   {id: 'C07', count: 1}
+// ])
+
 export const login = (user, password) => dispatch => {
   console.log('init')
   fake.login(user, password)
