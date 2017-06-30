c4f2c800e15d9491639f10733326963ae89e50e7
Iddar <iddar@dbug.mx>
2017-06-30 16:34:02 -0500

    fix

--- actions/api.js
+++ actions/api.js
@@ -9,10 +9,8 @@ import * as fake from '../lib/fakeAPI'
 // ])
 
 export const login = (user, password) => dispatch => {
-  console.log('init')
   fake.login(user, password)
     .then(user => {
-      dispatch({type: 'login', payload: user})
-      console.log(user)
+      dispatch({type: 'resolveLogin', payload: user})
     })
 }
